f9fe4160b515caf3806db0b76d3e42ff6be9c00c
wuenshen <wuenshen@hitour.cc>
2018-07-24 18:58:05 +0800

    fix(route): 修改路由结构

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+// routers
+import others from './others'
 
 Vue.use(Router)
 
+const routes = [].concat(others)
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes
 })
---
+++ template/src/router/others.js
@@ -0,0 +1,5 @@
+export default [{
+  path: '/home',
+  name: 'home',
+  component: resolve => require(['@/page/home/home'], resolve)
+}]
